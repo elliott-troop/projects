b1e72ae29192f7ba1d88a22ea6d1252bd8d3723b
Elliott Porter Jr <eporterjr@NY2-CogMAC-01.fios-router.home>
2018-10-15 19:40:12 -0400

    ListView and Adapter added to Sunshine app

--- Sunshine/app/src/main/java/com/example/eporterjr/sunshine/ui/main/MainFragment.java
+++ Sunshine/app/src/main/java/com/example/eporterjr/sunshine/ui/main/MainFragment.java
@@ -8,12 +8,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import com.example.eporterjr.sunshine.R;
 
 public class MainFragment extends Fragment {
 
     private MainViewModel mViewModel;
+    private ArrayAdapter<String> mForecastAdapter;
 
     public static MainFragment newInstance() {
         return new MainFragment();
@@ -23,7 +29,27 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_fragment, container, false);
+
+        View rootView = inflater.inflate(R.layout.main_fragment, container, false);
+        String[] forecastArray = {
+                "Today - Sunny - 88/63",
+                "Tomorrow - Sunny - 88/63",
+                "Wednesday - Sunny - 88/63",
+                "Thursday - Sunny - 88/63",
+                "Friday - Sunny - 88/63",
+                "Saturday - Sunny - 88/63",
+                "Sunday - Sunny - 88/63"
+        };
+
+        List<String> weekForecast = new ArrayList<>(Arrays.asList(forecastArray));
+
+        mForecastAdapter = new ArrayAdapter<>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textView, weekForecast);
+
+        ListView listView = rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+
+        return rootView;
+        //return inflater.inflate(R.layout.main_fragment, container, false);
     }
 
     @Override
